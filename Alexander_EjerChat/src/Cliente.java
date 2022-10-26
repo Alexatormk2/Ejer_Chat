c38803d66ca1dc58c5f803fdab1916d82a221e9f
alexander <alexander.regalado@ikasle.egibide.org>
2022-10-26 21:21:35 +0200

    emepzando el chat

--- Alexander_EjerChat/src/Cliente.java
+++ Alexander_EjerChat/src/Cliente.java
@@ -1,2 +1,52 @@
+import javax.sound.midi.Soundbank;
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+
 public class Cliente {
+
+    static String host = "localhost";
+    static int puerto = 5382;
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Socket socket = new Socket(host, puerto);
+        ObjectInputStream leer = new ObjectInputStream(socket.getInputStream());
+        ObjectOutputStream escribir = new ObjectOutputStream(socket.getOutputStream());
+        Persona p = new Persona();
+        System.out.println("Nombre de usuario");
+        String nombre = br.readLine();
+        p.setNombre(nombre);
+        p.setMensaje(p.getNombre() + " se a conectado");
+
+        escribir.writeObject(p);
+        int opcion;
+        do {
+            System.out.println("Menu");
+            System.out.println("1.Escribir en chat");
+            System.out.println("2.Salir");
+            opcion = Integer.parseInt(br.readLine());
+
+
+            switch (opcion) {
+
+                case 1:
+                    System.out.println("Escribe algo:");
+                    String paloma = br.readLine();
+                    p.setMensaje(p.getNombre() + ": " + paloma);
+                    escribir.writeObject(p);
+
+                    break;
+
+
+            }
+
+        } while (opcion != 2);
+        socket.close();
+
+
+    }
+
 }
--- Alexander_EjerChat/src/Servidor.java
+++ Alexander_EjerChat/src/Servidor.java
@@ -1,15 +1,21 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.SQLOutput;
 
 public class Servidor {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         ServerSocket socket = new ServerSocket(5382);
         Socket cliente = socket.accept();
 
-        InputStreamReader leer = new InputStreamReader(cliente.getInputStream());
+
+        ObjectInputStream leer = new ObjectInputStream(cliente.getInputStream());
         OutputStreamWriter escribir = new OutputStreamWriter(cliente.getOutputStream());
-        
+
+        Persona p;
+        System.out.println("--------CHAT-------");
+        p = (Persona) leer.readObject();
+        System.out.println(p.getMensaje());
 
 
     }
